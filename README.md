6ef2df5943647c56051ced084a2d36e1506b8bbc
Steven Estrella <se@shearspire.com>
2019-02-11 10:35:15 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Lorem Generator
+> A simple little program to generate nonsense greeking to use in your layouts.
+
+## Documentation
+
+- Type the number of words you need into the number input.
+- Click the 'Get Nonsense' button to generate the greeking text.
+- Each time you click the 'Get Nonsense' button a new random string will be generated.
---
+++ lorem.js
@@ -0,0 +1,53 @@
+(function(){
+	let makebtn, input, output;
+	function init(){
+	  makebtn = document.getElementById("makebtn");
+	  input = document.getElementById("wordcount");
+	  output = document.getElementById("blurb");
+	  makebtn.addEventListener("click",showNonsense,false);
+	  renderNonsense(input.value);
+	  document.removeEventListener("DOMContentLoaded", init);
+	}
+	function showNonsense(e){
+	  renderNonsense(input.value);
+	}
+	function renderNonsense(num){
+	  output.innerHTML = LoremMaker.getLorem(num);
+	}
+	const LoremMaker = function(){
+		let words = ["a","ac","accumsan","aenean","aliquam","aliquet","amet","ante","arcu","at","auctor","augue","bibendum","commodo","condimentum","congue","consectetur","consequat","convallis","cras","curabitur","cursus","dapibus","diam","dictum","dignissim","dis","dolor","donec","dui","duis","efficitur","egestas","eget","eleifend","elementum","elit","enim","erat","eros","est","et","etiam","eu","euismod","ex","facilisi","facilisis","fames","faucibus","felis","fermentum","feugiat","finibus","fringilla","fusce","habitant","hendrerit","id","illegitimi","imperdiet","in","integer","ipsum","justo","lacinia","lacus","laoreet","lectus","leo","libero","ligula","lobortis","lorem","luctus","maecenas","magna","magnis","malesuada","mattis","mauris","maximus","mentus","metus","mi","molestie","mollis","montes","morbi","mus","nam","nascetur","natoque","nec","neque","netus","nibh","nisi","nisl","non","noncompis","nulla","nullam","nunc","odio","orci","ornare","parturient","pellentesque","penatibus","phasellus","placerat","porta","porttitor","posuere","pretium","proin","pulvinar","purus","quam","quis","quisque","rhoncus","ridiculus","risus","rutrum","sagittis","sapien","scelerisque","sed","semper","senectus","sit","sodales","sollicitudin","stupido","suscipit","suspendisse","tellus","tempus","tincidunt","tortor","tristique","turpis","ullamcorper","ultrices","ultricies","urna","ut","varius","vehicula","vel","velit","venenatis","vestibulum","vitae","vivamus","viverra","volutpat","vulputate"];
+		function getLorem(count = 0){
+		count = Number(count);
+			let nonsense = "";
+		let warr = [];
+			let len = words.length;
+			count = count > 0 ? count : randRange(30,len-1);
+	
+			for (let i=0;i<count;i++){
+				let randomWord = words[randRange(0,len-1)];
+		  warr.push(randomWord);
+			}
+		for (let i=0;i<count;i++){
+		  if (i % 10 === 0){
+			warr[i] = warr[i].substring(0,1).toUpperCase() + warr[i].substring(1);
+			 if (i>=150 && i % 150 === 0){
+			   warr[i] = "<br><br>" + warr[i];
+			 }
+		  }
+		  if (i % 10 === 9 || i === count - 1){
+			warr[i] = warr[i] + ".";
+		  }
+		}
+		nonsense = warr.join(" ");
+	
+		return nonsense;
+		}
+		//random number utility function
+		function randRange(min, max) {
+			return Math.floor(Math.random()*(max-min+1))+min;
+		}
+		return {"getLorem":getLorem}
+	}();
+	
+	document.addEventListener("DOMContentLoaded", init);
+}());
