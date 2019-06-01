eec591025b6a2ff7c7d7768fada105c7f810b3dd
Steven Estrella <se@shearspire.com>
2019-06-01 19:03:11 -0400

    Reworked LoremMaker as a module to generate phrases from Cicero.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import { LoremMaker } from './loremmaker.js';
+
 (function(){
 	let makebtn, copybtn, input, output, blurbcopy, sonnets, sonnetbtn, titlehead;
 	function init(){
@@ -11,7 +13,7 @@
 		copybtn.addEventListener("click",copyToClipboard,false);
 		sonnetbtn = document.getElementById("sonnetbtn");
 		sonnetbtn.addEventListener("click",showSonnet,false);
-		renderNonsense(input.value);
+		renderNonsense(-1);
 		document.removeEventListener("DOMContentLoaded", init);
 		document.getElementById("closealert").addEventListener("click",function(){
 			document.getElementById("copydialog").style.display = "none";
@@ -28,6 +30,7 @@
 			 input.value = loremObject.count;
 		 }
         blurbcopy.value = loremObject.blurb.split("<br>").join("\n");
+		input.value = blurbcopy.value.split(" ").length;
 	}
     function copyToClipboard() {
         blurbcopy.select();
@@ -35,34 +38,6 @@
         document.getElementById("copydialog").style.display = "block";
     }
 
-	const LoremMaker = function(){
-		let words = ["a","ac","accumsan","aenean","aliquam","aliquet","amet","ante","arcu","at","auctor","augue","bibendum","commodo","condimentum","congue","consectetur","consequat","convallis","cras","curabitur","cursus","dapibus","diam","dictum","dignissim","dis","dolor","donec","dui","duis","efficitur","egestas","eget","eleifend","elementum","elit","enim","erat","eros","est","et","etiam","eu","euismod","ex","facilisi","facilisis","fames","faucibus","felis","fermentum","feugiat","finibus","fringilla","fusce","habitant","hendrerit","id","illegitimi","imperdiet","in","integer","ipsum","justo","lacinia","lacus","laoreet","lectus","leo","libero","ligula","lobortis","lorem","luctus","maecenas","magna","magnis","malesuada","mattis","mauris","maximus","mentus","metus","mi","molestie","mollis","montes","morbi","mus","nam","nascetur","natoque","nec","neque","netus","nibh","nisi","nisl","non","noncompis","nulla","nullam","nunc","odio","orci","ornare","parturient","pellentesque","penatibus","phasellus","placerat","porta","porttitor","posuere","pretium","proin","pulvinar","purus","quam","quis","quisque","rhoncus","ridiculus","risus","rutrum","sagittis","sapien","scelerisque","sed","semper","senectus","sit","sodales","sollicitudin","stupido","suscipit","suspendisse","tellus","tempus","tincidunt","tortor","tristique","turpis","ullamcorper","ultrices","ultricies","urna","ut","varius","vehicula","vel","velit","venenatis","vestibulum","vitae","vivamus","viverra","volutpat","vulputate"];
-		function getLorem(count = 0){
-			count = Number(count);
-			let warr = [];
-			let len = words.length;
-			count = count > 0 ? count : randRange(30,len-1);
-	
-			for (let i=0;i<count;i++){
-				let randomWord = words[randRange(0,len-1)];
-		  		warr.push(randomWord);
-			}
-			for (let i=0;i<count;i++){
-			  if (i % 10 === 0){
-				warr[i] = warr[i].substring(0,1).toUpperCase() + warr[i].substring(1);
-				 if (i>=150 && i % 150 === 0){
-				   warr[i] = "<br><br>" + warr[i];
-				 }
-			  }
-			  if (i % 10 === 9 || i === count - 1){
-				warr[i] = warr[i] + ".";
-			  }
-			}
-			return {"blurb":warr.join(" "),"count":count};
-		}
-
-		return {"getLorem":getLorem}
-	}();
 	//random number utility function
 	function randRange(min, max) {
 		return Math.floor(Math.random()*(max-min+1))+min;
